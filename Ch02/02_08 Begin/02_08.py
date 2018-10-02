f9bd40e83c2e21658507b86d9e3809be965e1728
Sam Pisheh <spisheh@berkeley.edu>
2018-10-02 10:37:11 -0700

    02_08 done

---
+++ Ch02/02_08 Begin/02_08.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+
+	ret, frame = cap.read()
+
+	frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+	cv2.imshow("Webcam", frame)
+
+
+
+
+
+	ch=cv2.waitKey(1)
+	if ch & 0xff == ord("q"):
+		break
+cap.release()
+cv2.destroyAllWindows()
